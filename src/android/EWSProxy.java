08d2158fe701d700c6cdf4a1147ecc205bb498de
sauvainr <sauvainr@hotmail.com>
2015-10-04 15:49:29 +0800

    jf14

--- src/android/EWSProxy.java
+++ src/android/EWSProxy.java
@@ -201,7 +201,7 @@ public class EWSProxy {
   public List<CalendarFolder> getCalendars(FolderId calId) {
     if(this.calendars == null) {
       FindFoldersResults findResults = this.service.findFolders(calId, new FolderView(Integer.MAX_VALUE));
-      this.calendars = findResults.folders;
+      this.calendars = findResults.getFolders();
     }
     return this.calendars;
   }
